635ab834abfd16680664bcf7d6e56805d891ab97
Dan <dan.chadfield@gmail.com>
2021-04-15 19:39:29 +0100

    Store message components

---
+++ messageGenerator.js
@@ -0,0 +1,24 @@
+const adjective = [
+    'pretty sick',
+    'absolutely mega',
+    'chossy',
+    'pumpy',
+    'crappy',
+    'nails hard'
+]
+const type = [
+    'techy face climb',
+    'roof',
+    'slab',
+    'crimpy overhang',
+    'diff',
+    '9a',
+    'V2'
+]
+const remark = [
+    'It goes boys!',
+    "We'll see.",
+    "Today's the day!",
+    'Too hard for me!',
+    'My first of the grade!'
+]
